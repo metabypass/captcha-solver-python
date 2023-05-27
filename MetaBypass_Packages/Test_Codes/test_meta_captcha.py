9f200babeae1ca6c5b43657a8549fb4190bc0358
Metabypass <128980891+metabypass@users.noreply.github.com>
2023-05-27 14:03:57 +0330

    Update test_meta_captcha.py

--- MetaBypass_Packages/Test_Codes/test_meta_captcha.py
+++ MetaBypass_Packages/Test_Codes/test_meta_captcha.py
@@ -1,9 +1,9 @@
-from metabypasscaptcha import *
+from metabypass import MetaBypass
+
 CLIENT_ID = 'YOUR_CLIENT_ID'  # ****CHANGE HERE WITH YOUR VALUE*******
 CLIENT_SECRET = 'YOUR_CLIENT_SECRET'  # ****CHANGE HERE WITH YOUR VALUE*******
 EMAIL = 'YOUR_ACCOUNT_EMAIL'  # ****CHANGE HERE WITH YOUR VALUE*******
 PASSWORD = 'YOUR_ACCOUNT_PASSWORD'  # ****CHANGE HERE WITH YOUR VALUE*******
-cred=getCredentials(CLIENT_ID,CLIENT_SECRET,EMAIL,PASSWORD)
-image_base64 = image_to_base64('YOUR_CAPTCHA_IMAGE_PATH')  # ****CHANGE HERE WITH YOUR VALUE*******
-captcha_rsponse = image_captcha(image_base64,cred)
-print(captcha_rsponse)
+solver=MetaBypass(CLIENT_ID,CLIENT_SECRET,EMAIL,PASSWORD)
+captcha_response = solver.image_captcha('YOUR_CAPTCHA_IMAGE_PATH')
+print(captcha_response)
