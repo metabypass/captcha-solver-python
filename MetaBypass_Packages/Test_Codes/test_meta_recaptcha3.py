91976c6bb88f70409a879ce824c75f197bc05c5f
Metabypass <128980891+metabypass@users.noreply.github.com>
2023-05-27 14:07:30 +0330

    Update test_meta_recaptcha3.py

--- MetaBypass_Packages/Test_Codes/test_meta_recaptcha3.py
+++ MetaBypass_Packages/Test_Codes/test_meta_recaptcha3.py
@@ -1,13 +1,14 @@
 # call reCAPTCHA v2 API
-from metabypassrecaptcha3 import *
+from metabypass import MetaBypass
 
 CLIENT_ID = 'YOUR_CLIENT_ID'  # ****CHANGE HERE WITH YOUR VALUE*******
 CLIENT_SECRET = 'YOUR_CLIENT_SECRET'  # ****CHANGE HERE WITH YOUR VALUE*******
 EMAIL = 'YOUR_ACCOUNT_EMAIL'  # ****CHANGE HERE WITH YOUR VALUE*******
 PASSWORD = 'YOUR_ACCOUNT_PASSWORD'  # ****CHANGE HERE WITH YOUR VALUE*******
-cred=getCredentials(CLIENT_ID,CLIENT_SECRET,EMAIL,PASSWORD)
+
+solver=MetaBypass(CLIENT_ID,CLIENT_SECRET,EMAIL,PASSWORD)
 
 site_url = "YOUR_SITE_URL"  # ****CHANGE HERE WITH YOUR VALUE*******
 site_key = "YOUR_SITE_KEY"  # ****CHANGE HERE WITH YOUR VALUE*******
-rc3 = reCAPTCHAV3(url=site_url, site_key=site_key,cred=cred)
+rc3 = solver.reCAPTCHAV3(url=site_url, site_key=site_key)
 print(rc3)
