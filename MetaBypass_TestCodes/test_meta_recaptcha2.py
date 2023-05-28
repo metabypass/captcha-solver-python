5dcd2b66047424dbb675e60967a6902cecd1bffd
Metabypass <128980891+metabypass@users.noreply.github.com>
2023-05-28 11:49:09 +0330

    Update test_meta_recaptcha2.py

--- MetaBypass_TestCodes/test_meta_recaptcha2.py
+++ MetaBypass_TestCodes/test_meta_recaptcha2.py
@@ -13,30 +13,5 @@ site_url = "YOUR_SITE_URL"  # ****CHANGE HERE WITH YOUR VALUE*******
 site_key = "YOUR_SITE_KEY"  # ****CHANGE HERE WITH YOUR VALUE*******
 
 
-rev2_response = solver.reCAPTCHAV2(url=site_url, site_key=site_key)
-try:
-    recaptcha_id = rev2_response['data']['RecaptchaId']
-except:
-    print('error!')
-    print(rev2_response)
-    exit()
-
-# ------------------------------ GET RESULT ----------------------------------
-
-# print('wait 10 seconds to get reCAPTCHA result')
-for i in range(6):
-    # wait 10 seconds to get result
-    time.sleep(10)
-
-    result = solver.getResult(recaptcha_id)
-
-    try:
-        if result:
-            # print(json.dumps(response))
-            break
-    except:
-        print(f"error! {result}")
-        pass
-
-    # print("reCAPTCHA result not ready. wait 10 seconds again ...");
-print(result)
+rev2_response = solver.reCAPTCHAV2(url=site_url, site_key=site_key)  
+print(rev2_response)
